31fae2c0ce3bca5d9d9a79058e69a825395b80e6
Ekaterina Karamacheva <76935364+karamacheva@users.noreply.github.com>
2021-01-21 18:25:30 +0200

    Add files via upload

---
+++ project20210120.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float celsius;
+    float fahrenheit = 0;
+    printf("-----------------------------------");
+    printf("\n   Celsius            Fahrenheit\n");
+    for (celsius = -50; celsius <= 150; celsius += 5)
+    {
+        printf("-----------------------------------");
+        fahrenheit = (celsius * 9 / 5) + 32;
+        printf("\n | %6.2f\370C\t |\t%6.2f\370F |\n", celsius, fahrenheit);
+    }
+    return 0;
+}
